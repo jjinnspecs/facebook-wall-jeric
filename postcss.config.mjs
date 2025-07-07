359e7477273651f9ce895d384e450197552e8013
jjinspecs <j3.cariaso@gmail.com>
2025-07-08 01:07:26 +0800

    Refactor: Update postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss", "autoprefixer"],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
